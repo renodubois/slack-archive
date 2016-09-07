81483b121eb204fbac909dfe9295d43a936a6761
renodubois <rtdm9c@mst.edu>
2016-09-07 14:20:17 -0500

    Added func. to return channel names

--- dirs.py
+++ dirs.py
@@ -4,4 +4,12 @@ from pathlib import Path
 # Takes no parameters, automatically searches the 'logs' folder.
 # Returns a list of dicts, for use with the jinja2 templating engine.
 def get_chan_names():
-    pass
+    p = Path('./logs')
+    chan_names = []
+    for f in p.iterdir():
+        if f.is_dir():
+            dir_name = str(f)
+            dir_name = dir_name[5:]
+            chan_names.append({'name':dir_name})
+
+    return chan_names
