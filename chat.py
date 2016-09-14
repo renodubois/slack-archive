1007856fda1d21077d79e9f39f9022443e22f049
renodubois <rtdm9c@mst.edu>
2016-09-14 10:15:12 -0500

    Working mentions_to_names, more comments
    Fixed the issue of mentions_to_names not converting all instances of
    mentions in a single line. Also added comments in chat.py to make the
    code a bit more clear.

--- chat.py
+++ chat.py
@@ -1,15 +1,13 @@
 import json
-from pathlib import Path
-#import slacker
 from api_tokens import devtoken
 
-#slack = Slacker(devtoken)
-
-path = "logs/actionone/2016-07-19.json"
 userspath = "logs/users.json"
 
-
-# Returns a list of dicts containing messages from a given channel (passed via parameter chan)
+'''
+    show_messages(chan)
+    Shows messages for a given channel.
+    Returns: a list of dicts containing messages from a given channel (passed via parameter chan)
+'''
 def show_messages(chan):
     # Create a list to hold chat data from every file.
     total_chat_data = []
@@ -27,14 +25,14 @@ def show_messages(chan):
             chat_data = json.load(raw_data)
         # Iterate through the messages and assign real names instead of user IDs    
         for msg in chat_data:
+            # Replaces the mentions with actual names
+            # First make sure that the text is an actual string
             if 'text' in msg and type(msg['text']) is not None:
-                    #print(type(msg['text']))
-                    #print(msg['text'])
+                if 'subtype' in msg:
+                    msg['text'] = mentions_to_names(msg['text'], msg['subtype'])
+                else:
                     msg['text'] = mentions_to_names(msg['text'], "")
             for usr in user_data:
-                # Replaces the mentions with actual names
-                # First make sure that the text is an actual string
-                
                 # Check to see if it's a user's post, or a bot post.
                 if 'bot_id' in msg:
                     msg['real_name'] = "bot"
@@ -50,19 +48,29 @@ def show_messages(chan):
                         # Add the user_name attribute, used for mouseover display
                         msg['user_name'] = usr['name']
         for msg in chat_data:
-            total_chat_data.append(msg)
+            if msg['text'] is not None:
+                total_chat_data.append(msg)
         # TODO: Change mentions to real names
-
         # TODO: Change 'ts' to a printable date
-            
-
-
     return total_chat_data
 
+'''
+    mentions_to_names(msg, msg_type)
+    Takes a message and message type and replaces any mentions with readble text.
+    If the msg_type matches any of the bad_types listed, just returns an empty string.
+    Returns: a string containing the message after mentions being replaced
+'''
 def mentions_to_names(msg, msg_type):
+    # A tuple of the types we want to ignore.
+    bad_types = ('channel_join', 'pinned_item', 'channel_name', )
+    # If the message has some type we don't want to deal with right now, so just remove them.
+    if not msg_type == "":
+        return ""
     # Length of a 'mention string' (includes '<', '>', the @ and the User ID) minus 1, since we're already counting for
     # the '<'.
     mention_len = 12
+    # Make a list for "replace data", used to replace the user ids at the end of the function.
+    replace_data = []
     # Open user files to get User Data
     with open(userspath) as raw_data:
         user_data = json.load(raw_data)
@@ -74,17 +82,20 @@ def mentions_to_names(msg, msg_type):
             # If we have a '@' after the '<', it's a user being mentioned.
             if msg[i+1] == '@':
                 user_id = msg[(i+2):(i+(mention_len-1))]
-                # If this message is referencing someone joining the channel, then the length is different.
-                if msg_type == 'channel_join':
-                    return ""
-                # Replace the .json mention with readable text.
-                print(msg[:i])
-                print(id_to_realname(user_id))    
-                new_msg = msg[:i] + '@' + id_to_realname(user_id) + msg[(i+mention_len):]
-                return new_msg
+                # Append the data (user id and index) of the mention to be replaced so that we can iterate through them later
+                replace_data.append((i, user_id))
+    # We have info regarding the stuff needing to be replaced, go through and replace it.
+    for msg_data in reversed(replace_data):
+        msg = msg[:msg_data[0]] + '@' + id_to_realname(msg_data[1]) + msg[(msg_data[0]+mention_len):]
+    # Return the replaced message.
     return msg
 
 
+''' 
+    id_to_realname(user_id)
+    Takes a string containing a user_id, returns the real name or name of the user, depending on availability.
+    Returns: a string containing real name or username
+'''
 def id_to_realname(user_id):
     with open(userspath) as raw_data:
         user_data = json.load(raw_data)
