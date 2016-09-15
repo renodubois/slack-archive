9f67d9120909999ba1ff9a62705dd9a85877b5d8
renodubois <rtdm9c@mst.edu>
2016-09-15 16:10:11 -0500

    Redid the get_chan_names() function
    Renamed, as well as added a ‘path’ key that contains the entire chan
    path. Also checks for hidden directories and ignores them.

--- dirs.py
+++ dirs.py
@@ -3,13 +3,13 @@ from pathlib import Path
 # Retrieve all of the channel names from the logs folder.
 # Takes no parameters, automatically searches the 'logs' folder.
 # Returns a list of dicts, for use with the jinja2 templating engine.
-def get_chan_names():
+def get_chan_info():
     p = Path('./logs')
     chan_names = []
     for f in p.iterdir():
-        if f.is_dir():
+        if f.is_dir() and not str(f)[6] == ".":
             dir_name = str(f)
+            dir_path = str(f)
             dir_name = dir_name[5:]
-            chan_names.append({'name':dir_name})
-
+            chan_names.append({'name':dir_name, 'path':dir_path})
     return chan_names
